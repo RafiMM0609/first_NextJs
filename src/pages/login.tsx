9d867c0119a6f8d49b59a033f44f5518314753ae
Rafi Mahrus <75168126+RafiMM0609@users.noreply.github.com>
2024-07-20 22:35:53 +0700

    login and about

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,36 +2,36 @@ import React, { useState, FormEvent } from 'react';
 import styles from './login.module.css'; // Assuming you use CSS modules
 
 const LoginPage: React.FC = () => {
-  const [firstName, setFirstName] = useState('John');
-  const [lastName, setLastName] = useState('Doe');
+  const [firstName, setFirstName] = useState('xixixr');
+  const [password, setLastName] = useState('password123');
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Prevent the default form submission
     // Handle form submission logic here
-    console.log('Submitted:', { firstName, lastName });
+    console.log('Submitted:', { firstName, password });
   };
 
   return (
     <main className={styles.container}>
       <form onSubmit={handleSubmit} className={styles.loginForm}>
         <div className={styles.formGroup}>
-          <label htmlFor="fname">First name:</label>
+          <label htmlFor="name">User Name</label>
           <input
             type="text"
-            id="fname"
-            name="fname"
+            id="name"
+            name="name"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             className={styles.input}
           />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="lname">Last name:</label>
+          <label htmlFor="Password">Password</label>
           <input
-            type="text"
-            id="lname"
-            name="lname"
-            value={lastName}
+            type="password"
+            id="Password"
+            name="Password"
+            value={password}
             onChange={(e) => setLastName(e.target.value)}
             className={styles.input}
           />
